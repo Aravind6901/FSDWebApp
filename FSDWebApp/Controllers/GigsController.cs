3828bf0b3250dfbc0beae104cbf02f21649ea20c
Aravind <Aravind@DESKTOP-4RSCUV3.(none)>
2018-10-13 23:25:43 +0530

    Added Labels and dropdown in Gigs page

---
+++ FSDWebApp/Controllers/GigsController.cs
@@ -0,0 +1,30 @@
+﻿using FSDWebApp.Models;
+using FSDWebApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FSDWebApp.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel = new GigFromViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
