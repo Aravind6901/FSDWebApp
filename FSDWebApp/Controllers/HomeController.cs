c38337a1c188e2f6f7a3376b524b404c0f2ac5d4
Aravind <Aravind@DESKTOP-4RSCUV3.(none)>
2018-10-13 22:19:06 +0530

    Trail

--- FSDWebApp/Controllers/HomeController.cs
+++ FSDWebApp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace FSDWebApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page..";
 
             return View();
         }
