185375d9844aaa0d924866d94c7e921c08db5c3f
Aravind <Aravind@DESKTOP-4RSCUV3.(none)>
2018-10-14 15:49:17 +0530

    move dateTime Calculation to GigFormViewModel

--- FSDWebApp/ViewModels/GigFromViewModel.cs
+++ FSDWebApp/ViewModels/GigFromViewModel.cs
@@ -11,6 +11,13 @@ namespace FSDWebApp.ViewModels
         public string Venue { get; set; }
         public string Date { get; set; }
         public string Time { get; set; }
+        public DateTime DateTime
+        {
+            get
+            {
+                return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+            }
+        }
         public byte Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
     }
